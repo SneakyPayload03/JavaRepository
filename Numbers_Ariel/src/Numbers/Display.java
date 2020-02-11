6261c43682e2166032adf97ec168a5d77b712016
Ariel Clark <S01227048@acad.tri-c.edu>
2020-02-10 19:32:50 -0500

    here

---
+++ Numbers_Ariel/src/Numbers/Display.java
@@ -0,0 +1,16 @@
+package Numbers;
+
+public class Display {
+    public static void displayTwiceTheNumber(int x){
+        int Product = x * 2;
+        System.out.println(Product);
+    }
+    public static void displayNumberPlusFive(int x) {
+        int Sum = x + 5;
+        System.out.println(Sum);
+    }
+    public static void displayNumberSquared(int x){
+        int Product = x * x;
+        System.out.println(Product);
+    }
+}
